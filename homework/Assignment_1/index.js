6ff6276c6c46d11dd536a1b69d75c67ee3c03a81
Steve Osazuwa <sosazuwa00@gmail.com>
2018-10-14 11:05:33 -0700

    NodeJS Assignment 1 init commit

---
+++ homework/Assignment_1/index.js
@@ -0,0 +1,95 @@
+/*
+    This module creates a simple HTTP service that:
+        1. Allows a user to post (interpreting this as any type or request) to /hello
+        2. Responds with a message in the form: {'message': 'some message'}
+
+    The service will say hi back.
+*/
+
+const http = require('http')
+const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
+const portForListening = 3000
+
+// Setup server
+server = http.createServer(function (req, resp) {
+    processRequest(req, resp);
+})
+
+server.listen(portForListening, function(){
+    console.log('Server listening on ', portForListening)
+});
+
+function processRequest(req, resp) {
+    // process URL
+    parsedUrl = url.parse(req.url, true);
+    path = parsedUrl.pathname;
+    trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Resolve request metadata
+    var queryStringObj = parsedUrl.query;
+    var headers = req.headers;
+    var method = req.method;
+
+    // Resolve payload
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    })
+
+    // Request end event handler
+    req.on('end', function(){
+        // Resolve any remaining payload in buffer
+        buffer += decoder.end();
+
+        // Choose handler, default to not found
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObj': queryStringObj,
+            'method': method,
+            'payload': buffer,
+            'headers': headers
+        };
+
+        // Route request
+        chosenHandler(data, function(statusCode, payload){
+            // Use handlers status code, or use default 200
+            // Use handler payload or use {}
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+            payload = typeof(payload) == 'object' ? payload : {};
+
+            // Convert payload to string
+            var payloadString = JSON.stringify(payload);
+
+            // Set content type to JSON
+            resp.setHeader('Content-Type', 'application/json');
+            resp.writeHead(statusCode);
+            resp.end(payloadString);
+
+            // Log what server did
+            console.log('Returning payload: ', payloadString);
+            console.log('Returning status code: ', statusCode, '\n');
+        });
+    })
+}
+
+
+// Define handlers
+const handlers = {}
+handlers.greet = function sayHiBackWiseauStyle(data, callback) {
+    console.log('Someone is saying hi to us.');
+    callback(200, {'message': 'Oh hai Mark!'});
+};
+
+handlers.notFound = function noValidEndPointFound(data, callback) {
+    callback(404);
+};
+
+
+router = {
+    'hi': handlers.greet
+};
